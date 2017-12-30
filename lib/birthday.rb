4fd6e2266b4eea545f8e47f9a3b3f682ff5c19d5
Bryan Ortiz <bryan@bryanortiz.com>
2017-12-30 21:10:34 +0000

    automatically backed up by learn

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -1,11 +1,12 @@
 # birthday_kids = {
-#   "Timmy" => 9, 
-#   "Sarah" => 6, 
+#   "Timmy" => 9,
+#   "Sarah" => 6,
 #   "Amanda" => 27
 # }
 
 def happy_birthday(birthday_kids)
   # add your code snippet here!
+birthday_kids.each do |key, value|
+  puts "Happy Birthday #{key}! You are now #{value}
+ years old!"
 end
-
-
--- lib/cruise_ship.rb
+++ lib/cruise_ship.rb
@@ -1,11 +1,12 @@
 # passengers = {
-# suite_a: "Amanda Presley", 
-# suite_b: "Seymour Hoffman", 
-# suite_c: "Alfred Tennyson", 
-# suite_d: "Charlie Chaplin", 
+# suite_a: "Amanda Presley",
+# suite_b: "Seymour Hoffman",
+# suite_c: "Alfred Tennyson",
+# suite_d: "Charlie Chaplin",
 # suite_e: "Crumpet the Elf"
 # }
 
 def select_winner(passengers)
   # add the code snippet here!
+  
 end
